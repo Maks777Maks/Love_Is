014faa3e304e47cddd0755c884614c589af1bef0
Maks Rogach <m.rogach777@gmail.com>
2019-04-03 17:38:26 +0300

    add app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,45 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { RegisterFormComponent } from './register-form/register-form.component';
-import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './COMPONENTS/register-form/register-form.component';
+import { LoginFormComponent } from './COMPONENTS/login-form/login-form.component';
+import { RegistrationComponent } from './COMPONENTS/registration/registration.component';
+
+import {ReactiveFormsModule} from '@angular/forms';
+import { ApiService } from './CORE/api.service';
+import { HttpClientModule } from '@angular/common/http';
+
+import { TokenInterceptor } from './CORE/interceptor';
+
+
+
+// Components PrimeNG
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {PasswordModule} from 'primeng/password';
+import {TabViewModule} from 'primeng/tabview';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponent,
+    LoginFormComponent,
     RegisterFormComponent,
-    LoginFormComponent
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    InputTextModule,
+    ButtonModule,
+    PasswordModule,
+    TabViewModule
   ],
-  providers: [],
+  providers: [ ApiService, TokenInterceptor],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
